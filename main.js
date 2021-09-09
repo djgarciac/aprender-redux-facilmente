c9e3346608dd7acf6b503e6d5a10031bd7e55c9b
David Garcia <davidjosue.garcia@outlook.com>
2021-09-08 21:39:51 -0600

    Actualizar un elemento del objeto de javascript

--- main.js
+++ main.js
@@ -2,7 +2,20 @@
 // Nodes
 let input = document.querySelector('#input');
 let lista = document.querySelector('#lista');
-let todos = {};
+let todos = {
+  0: {
+    text: 'Ir al cine',
+    done: false
+  },
+  1: {
+    text: 'Cenar',
+    done: true
+  },
+  2: {
+    text: 'Grabar',
+    done: false
+  }
+};
 
 // Functions
 const drawTodos = () => {
@@ -10,22 +23,38 @@ const drawTodos = () => {
 
   for (let key in todos) {
     let li = document.createElement('li');
-    li.id = key;
+    // li.id = key;
+
+    const classDone = todos[key].done ? 'done' : ''
     li.innerHTML = `
-      <span>${todos[key]}</span>
-      <span id="${key}">x</span>
+      <span id="${key}" class="${classDone}">${todos[key].text}</span>
+      <span>x</span>
     `;
+    setListeners(li);
     lista.appendChild(li);
   }
 };
+
+const setListeners = (li) => {
+  li.addEventListener('click', (e) => {
+    const { id: key } = e.target;
+    todos[key].done = !todos[key].done;
+    drawTodos();
+  });
+}
 
 // Listeners
 input.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
     let text = e.target.value;
     let id = Object.keys(todos).length;
-    todos[id] = text;
-
+    todos[id] = { text, done: false };
     drawTodos();
+    e.target.value = ''
   }
+});
+
+// Init
+document.addEventListener('DOMContentLoaded', () => {
+  drawTodos();
 });
