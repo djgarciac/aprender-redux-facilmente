00700980b7920908186883943d98f7f1f6e96be3
David Garcia <davidjosue.garcia@outlook.com>
2021-09-08 21:17:21 -0600

    Construyendo el app con Vanilla JS

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
 // import * as Redux from 'redux'
+// Nodes
+let input = document.querySelector('#input');
+let lista = document.querySelector('#lista');
+let todos = {};
 
+// Functions
+const drawTodos = () => {
+  lista.innerHTML = '';
 
+  for (let key in todos) {
+    let li = document.createElement('li');
+    li.id = key;
+    li.innerHTML = `
+      <span>${todos[key]}</span>
+      <span id="${key}">x</span>
+    `;
+    lista.appendChild(li);
+  }
+};
+
+// Listeners
+input.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    let text = e.target.value;
+    let id = Object.keys(todos).length;
+    todos[id] = text;
+
+    drawTodos();
+  }
+});
---
+++ src/main.js
@@ -0,0 +1,3 @@
+// import * as Redux from 'redux'
+
+console.log('Hello world!')
